ebce4299e29e6c0178c36cf476701da40e61a093
Lucas <lucasvinicios-fs@hotmail.com>
2019-01-24 10:19:20 -0200

    Initial project

---
+++ src/main/java/com/example/demo/RestApiJava8SpringMongoDbHerokuSwagger2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiJava8SpringMongoDbHerokuSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiJava8SpringMongoDbHerokuSwagger2Application.class, args);
+	}
+
+}
+
