4a6b9eb0988a86d18a26f8444894d7ba2b1a412a
Lucas <lucasvinicios-fs@hotmail.com>
2019-03-30 09:18:57 -0300

    Repository class Company

--- src/main/java/com/example/demo/Model/CompanyRepository.java
+++ src/main/java/com/example/demo/Model/CompanyRepository.java
@@ -4,6 +4,7 @@
 package com.example.demo.Model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.Entity.Company;
 
@@ -11,6 +12,7 @@ import com.example.demo.Entity.Company;
  * @author Lucas
  *
  */
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 	
 	Company findByCnpj(String cnpj);
