479f9d2dd8dabc928da9a9831e43677e3f19d570
Lucas <lucasvinicios-fs@hotmail.com>
2019-04-07 19:41:32 -0300

    First api method (GET)

--- src/main/java/com/example/demo/Controller/CompanyController.java
+++ src/main/java/com/example/demo/Controller/CompanyController.java
@@ -1,17 +1,16 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/company")
+@RequestMapping("/company")
 public class CompanyController {
 	
-	@GetMapping(value="/{cnpj}")
+	@RequestMapping("/getCompanybyCNPJ/{cnpj}")
 	public String getCompanybyCNPJ(@PathVariable("cnpj") String cnpj) {
-		return "Empresa de cnpj:" + cnpj;	
+		return "Empresa de cnpj:"+ cnpj;	
 	}
 
 }
--- src/main/java/com/example/demo/RestApiJava8SpringMongoDbHerokuSwagger2Application.java
+++ src/main/java/com/example/demo/RestApiJava8SpringMongoDbHerokuSwagger2Application.java
@@ -2,9 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan
 @SpringBootApplication
-public class RestApiJava8SpringMongoDbHerokuSwagger2Application {
+public class RestApiJava8SpringMongoDbHerokuSwagger2Application  {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiJava8SpringMongoDbHerokuSwagger2Application.class, args);
