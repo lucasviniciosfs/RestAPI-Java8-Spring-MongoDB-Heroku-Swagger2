f6e8b819889cddcc1af6dd24ca06ca5286e8e7f0
Lucas <lucasvinicios-fs@hotmail.com>
2019-07-20 15:13:14 -0300

    Using the service class

--- src/main/java/com/example/demo/Controller/CompanyController.java
+++ src/main/java/com/example/demo/Controller/CompanyController.java
@@ -1,15 +1,27 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Service.CompanyService;
+
+/**
+ * 
+ * @author Lucas
+ * Class used to receive the requests about Company
+ */
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
 	
+	@Autowired
+	CompanyService companyService;
+	
 	@RequestMapping("/getCompanybyCNPJ/{cnpj}")
 	public String getCompanybyCNPJ(@PathVariable("cnpj") String cnpj) {
+		companyService.testarServico();
 		return "Empresa de cnpj:"+ cnpj;	
 	}
 
