25b3bfc719de02a6b6748c57e4ba89367619addd
Lucas <lucasvinicios-fs@hotmail.com>
2019-07-20 16:04:43 -0300

    Adding a post method do company

--- src/main/java/com/example/demo/Controller/CompanyController.java
+++ src/main/java/com/example/demo/Controller/CompanyController.java
@@ -1,10 +1,14 @@
 package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.DTO.CompanyDTO;
 import com.example.demo.Service.CompanyService;
 
 /**
@@ -23,6 +27,12 @@ public class CompanyController {
 	public String getCompanybyCNPJ(@PathVariable("cnpj") String cnpj) {
 		companyService.testarServico();
 		return "Empresa de cnpj:"+ cnpj;	
+	}
+	
+	@PostMapping
+	public ResponseEntity<CompanyDTO> singupCompany(@RequestBody CompanyDTO companyDTO){
+		companyDTO.setId(1L);
+		return ResponseEntity.ok(companyDTO);
 	}
 
 }
