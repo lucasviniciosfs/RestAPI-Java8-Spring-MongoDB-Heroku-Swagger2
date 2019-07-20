863ddc3d70b20723f0fb574e018ec787cd0914ce
Lucas <lucasvinicios-fs@hotmail.com>
2019-07-20 15:13:49 -0300

    Adding a comment to class

--- src/main/java/com/example/demo/Service/CompanyService.java
+++ src/main/java/com/example/demo/Service/CompanyService.java
@@ -1,7 +1,12 @@
-package com.example.demo.Model;
+package com.example.demo.Service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * 
+ * @author Lucas
+ * Class used to contains the logical
+ */
 @Service
 public class CompanyService {
 	
